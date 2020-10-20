f7e91fb306abe949f6fa7ad7e9cb61d52d6f0c59
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-10-20 10:47:34 +0100

    Update type

--- modules/db_option_group/variables.tf
+++ modules/db_option_group/variables.tf
@@ -38,7 +38,7 @@ variable "major_engine_version" {
 
 variable "options" {
   description = "A list of Options to apply"
-  type        = list([map(any),map(any)])
+  type        = list[map(any),map(any)]
   default     = [
     {
       option_name = "STATSPACK"
